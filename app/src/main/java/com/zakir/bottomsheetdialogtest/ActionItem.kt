f3c36290118349d19cf911fa2aaf2bd0f79b3a09
Zakir Hossain <zakir01913@gmail.com>
2019-04-14 21:44:16 +0600

    Update other action icon

--- app/src/main/java/com/zakir/bottomsheetdialogtest/ActionItem.kt
+++ app/src/main/java/com/zakir/bottomsheetdialogtest/ActionItem.kt
@@ -3,5 +3,5 @@ package com.zakir.bottomsheetdialogtest
 enum class ActionItem(val textRes: Int, val iconRes: Int) {
     EMAIL(R.string.email, R.drawable.ic_email_black),
     SMS(R.string.sms, R.drawable.ic_message_black),
-    OTHER(R.string.other, R.drawable.ic_share_black)
+    OTHER(R.string.other, R.drawable.icon_more_horiz_black)
 }
